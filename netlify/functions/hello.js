87f9f05a9e758e1daac3c6b0b2e1442314413acc
Ronak Hapaliya <68993141+Ronak3899@users.noreply.github.com>
2024-10-26 15:07:54 +0530

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,27 +1,18 @@
 exports.handler = async (event) => {
-    const referrer = event.headers.referer;
+    const referrer = event.queryStringParameters.referrer; // Get referrer from query params
     const allowedDomain = 'https://creatorwala.in/';
     const canAccess = referrer && referrer.includes(allowedDomain);
 
     const headers = {
-        'Access-Control-Allow-Origin': '*', // Change to your domain for production
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
     };
 
-    if (canAccess) {
-        return {
-            statusCode: 200,
-            headers,
-            body: JSON.stringify({ canAccess }),
-        };
-    } else {
-        return {
-            statusCode: 200,
-            headers,
-            body: JSON.stringify({
-                canAccess,
-                message: referrer ? "Access denied: Your referrer is not allowed." : "Access denied: No referrer detected.",
-            }),
-        };
-    }
+    return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({
+            canAccess,
+        }),
+    };
 };
